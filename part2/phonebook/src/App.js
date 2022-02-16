a134a2facf35a6634cdfd5fa97eecbbe9c1539d2
user47 <KarthikShenoy@users.noreply.github.com>
2022-02-15 19:52:36 -0600

    2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [nameSearch, setNameSearch] = useState('')
   const [filteredPeople, setFilteredPeople] = useState(persons)
 
+  
+  const personHook = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log("Got response ", response)
+        setPersons(response.data)
+        setFilteredPeople(response.data)
+      })
+  }
+  useEffect(personHook, [])
   const handleNameUpdate = (event) => {
     setNewName(event.target.value)
   }
