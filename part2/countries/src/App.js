11dc99954ff62859379d2757aaa019cf0c24c239
user47 <KarthikShenoy@users.noreply.github.com>
2022-02-16 23:35:09 -0600

    2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios'
 
-const showCountryDetails =(element, resultHandler)=>{
+const api_key = process.env.REACT_APP_API_KEY
+
+const showCountryDetails = (element, resultHandler) => {
   resultHandler([element])
 }
 const List = ({ header, elements }) => {
@@ -14,6 +16,24 @@ const List = ({ header, elements }) => {
     </>
   )
 }
+const Weather = ({ capital }) => {
+  const [weather, setWeather] = useState("")
+  const weather_api_query = `http://api.weatherapi.com/v1/current.json?key=${api_key}&q=${capital}&aqi=no`
+  const get_weather = () => axios.get(weather_api_query).then(response => {
+    setWeather(response.data)
+  })
+  useEffect(get_weather, [weather_api_query])
+  if (weather === "") {
+    return <></>
+  }
+  return (
+    <>
+      <p>Temperature {weather.current.temp_c} Celcius</p>
+      <img src={weather.current.condition.icon} />
+      <p>wind {weather.current.wind_kph} KM/h</p>
+    </>
+  )
+}
 const CountryDetails = ({ country }) => {
   return (
     <>
@@ -22,6 +42,7 @@ const CountryDetails = ({ country }) => {
       <p>area {country.area}</p>
       <List header="languages" elements={Object.values(country.languages)} />
       <img src={country.flags['svg']} />
+      <Weather capital={country.capitalInfo.latlng} />
     </>
   )
 }
@@ -32,7 +53,7 @@ const QueryResult = ({ result, resultHandler }) => {
     return (
       <>
         {result.map(element =>
-            <p>{element.name.common}<button onClick={((e)=> showCountryDetails(element, resultHandler))}>Show</button></p>
+          <p key={element.name.common}>{element.name.common}<button onClick={((e) => showCountryDetails(element, resultHandler))}>Show</button></p>
         )}
       </>
     )
@@ -44,12 +65,9 @@ const App = () => {
   const [apiResult, setApiResult] = useState([])
   const [result, setResult] = useState([])
 
- 
-
   const fetchCountryHook = () => {
     axios.get("https://restcountries.com/v3.1/all")
       .then(response => {
-        console.log("Got data ");
         setApiResult(response.data)
       }
       )
@@ -59,11 +77,9 @@ const App = () => {
     event.preventDefault();
     const inp = event.target.value.toLowerCase()
     setQuery(inp)
-    apiResult.map(entry => console.log(entry.name.common))
     const res = apiResult.filter(country => {
       return country.name.common.toLowerCase().includes(inp)
     })
-    console.log(`Filtered data '${inp}' is '${res}'`)
     setResult(res)
   }
   return <>
@@ -72,7 +88,7 @@ const App = () => {
         find countries<input value={query} onChange={onQueryChange}></input>
       </div>
     </form>
-    <QueryResult result={result} resultHandler={setResult}/>
+    <QueryResult result={result} resultHandler={setResult} />
   </>
 }
 export default App;
