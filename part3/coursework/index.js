4119637061c8d2020005cd3d7b0ae6b3d9343038
user47 <KarthikShenoy@users.noreply.github.com>
2022-09-11 12:34:24 -0500

    Implement error handling middleware and fix the middleware ordering

--- part3/coursework/index.js
+++ part3/coursework/index.js
@@ -5,8 +5,6 @@ const cors = require('cors')
 const Note = require('./models/note')
 
 const password = process.env.MONGO_PASSWORD
-console.log(`Password loaded is ${password}`)
-
 
 let notes = [
   {
@@ -36,11 +34,15 @@ const requestLogger = (request, response, next)=>{
   console.log('---')
   next()
 }
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
+app.use(express.static('build'))
 app.use(express.json())
 app.use(requestLogger)
 app.use(cors())
-app.use(express.static('build'))
+// // handler of requests with unknown endpoint
 
 app.get('/', (request, response) => {
   response.send(
@@ -52,8 +54,17 @@ app.get('/api/notes/', (request, response) => {
     response.json(res)
   })
 })
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => response.json(note))
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+  .then(note =>{
+    if(note){
+      response.json(note)
+    }
+    else{
+      response.status(404).end()
+    }
+  })
+  .catch(error => next(error))
 })
 app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
@@ -80,7 +91,15 @@ app.post('/api/notes', (request, response) => {
     response.json(savedNote)
   })
 })
-
+const errorHandler = (error, request, response, next) => {
+  console.error(`from error handler ${error.message}`);
+  if(error.name === 'CastError' || error.name === 'BSONTypeError') {
+    return response.status(400).send({error: 'malformatted id'})
+  }
+  next(error)
+}
+app.use(unknownEndpoint)
+app.use(errorHandler)
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
